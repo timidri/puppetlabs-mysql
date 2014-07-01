e77b992d113a4d7670aac672d18b1051552d2830
Dimitri Tischenko <timidri@gmail.com>
2014-07-01 17:13:39 +0200

    added optional create_backup_user parameter

--- manifests/server/backup.pp
+++ manifests/server/backup.pp
@@ -16,21 +16,26 @@ class mysql::server::backup (
   $time = ['23', '5'],
   $postscript = false,
   $execpath   = '/usr/bin:/usr/sbin:/bin:/sbin',
+  $create_backup_user = true,
 ) {
 
-  mysql_user { "${backupuser}@localhost":
-    ensure        => $ensure,
-    password_hash => mysql_password($backuppassword),
-    provider      => 'mysql',
-    require       => Class['mysql::server::root_password'],
-  }
+  if $create_backup_user {
 
-  mysql_grant { "${backupuser}@localhost/*.*":
-    ensure     => present,
-    user       => "${backupuser}@localhost",
-    table      => '*.*',
-    privileges => [ 'SELECT', 'RELOAD', 'LOCK TABLES', 'SHOW VIEW', 'PROCESS' ],
-    require    => Mysql_user["${backupuser}@localhost"],
+    mysql_user { "${backupuser}@localhost":
+      ensure        => $ensure,
+      password_hash => mysql_password($backuppassword),
+      provider      => 'mysql',
+      require       => Class['mysql::server::root_password'],
+    }
+
+    mysql_grant { "${backupuser}@localhost/*.*":
+      ensure     => present,
+      user       => "${backupuser}@localhost",
+      table      => '*.*',
+      privileges => [ 'SELECT', 'RELOAD', 'LOCK TABLES', 'SHOW VIEW', 'PROCESS' ],
+      require    => Mysql_user["${backupuser}@localhost"],
+    }
+
   }
 
   cron { 'mysql-backup':
